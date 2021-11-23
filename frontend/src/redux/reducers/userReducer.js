ac0c7c17a0e1707f9dfc23adcfe1d4f1684f6430
Eugene Korir <korir16071@gmail.com>
2021-11-23 17:22:47 +0300

    Modified usersReducer.js

--- frontend/src/redux/reducers/userReducer.js
+++ frontend/src/redux/reducers/userReducer.js
@@ -2,48 +2,55 @@ import { LOGIN_FAIL, LOGIN_STATUS, LOGIN_SUCCESS, SIGNUP_FAIL, SIGNUP_STATUS, SI
 
 const initialState = {
     loading: false,
-    error: ""
+    error: "",
+    message: ""
 
 }
 
-const userReducer = (state=initialState,action)=>{
-    switch (action.type) {
+const userReducer = (state=initialState,{type,payload,message})=>{
+    switch (type) {
         case LOGIN_STATUS:
             return   {
                 ...state,
                 loading: true
             }
         case LOGIN_SUCCESS:
+            console.log("Login successful!");
             return {
-                ...state
-
+                ...state,
+                loading: false
             }
         case LOGIN_FAIL:
             return {
                 ...state,
-                error: action.payload
+                loading: false,
+                error: payload,
+                message: message
 
             }
-            case SIGNUP_STATUS:
-                return {
-                    ...state,
-                    loading: true
-                }
-        case SIGNUP_SUCCESS:
-            console.log("Signup succesful!");
+        case SIGNUP_STATUS:
             return {
-                ...state
+                ...state,
+                loading: true
+            }
+        case SIGNUP_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                message: message
 
             }
         case SIGNUP_FAIL:
             console.log("Signup failed!");
             return {
                 ...state,
-                error: action.payload
+                loading: false,
+                error: payload,
+                message: message
 
             }
         default:
-            break;
+            return state;
     }
 }
 
