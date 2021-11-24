61ab1f292678c7255e2a5dc3fde458bd365eebeb
Eugene Korir <korir16071@gmail.com>
2021-11-24 17:37:31 +0300

    cleaned up server.js

--- backend/server.js
+++ backend/server.js
@@ -1,68 +1,13 @@
 require('dotenv').config()
 const express = require('express')
-const {auth} = require('./controllers/authController')
-const { getUsers, addUser, loginUser } = require('./controllers/usersController')
 const PORT = process.env.PORT
 const cors = require('cors')
+const router = require('./routes/userRoutes')
 
 const app = express()
 app.use(express.json(),cors())
 
-
-
-app.get("/",(req,res)=>{
-    res.status(200).send("Welcome Home")
-    console.log('welcome home');
-})
-
-app.get("/users",(req,res)=>{
-    try {
-        getUsers().then((result,error)=>{
-            if (error) {
-                return error.message                
-            }
-            console.log(result);
-            return res.send(result)
-        })
-        
-    } catch (error) {
-        console.log(error.message);
-    }
-})
-
-app.post("/register",(req,res)=>{
-
-    const {username,fullname, email,password} = req.body
-    try {
-        addUser(username,fullname,email,password).then((result,err)=>{
-            if (err) {
-                res.send(err.message)
-                throw err
-            }
-            res.status(201).send("User Added Successfully!")
-        })
-    } catch (error) {
-        res.send(error)
-        console.log(error.message);
-        
-    }
-})
-
-app.post("/login",auth,(req,res)=>{
-    const {username,password} = req.body
-    console.log("Logging in");
-    try {
-        loginUser(username,password).then((result,error)=>{
-            if(error){
-                res.send(error)
-                return error
-            }
-        })
-    } catch (error) {
-        console.log(error.message);
-        
-    }
-})
+app.use("/",router)
 
 app.listen(PORT, ()=>{
     console.log(`App running on port ${PORT}`);
