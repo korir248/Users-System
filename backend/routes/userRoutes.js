66fb36e9e883a71e4c1be862e21d2302c1f696c0
Eugene Korir <korir16071@gmail.com>
2021-11-24 17:35:21 +0300

    Modified routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,13 @@
 const express = require('express')
-const { getUsers } = require('../controllers/usersController')
+const { getUsers, loginUser, addUser } = require('../controllers/usersController')
 const router =  express.Router()
 
 
 router.route("/users").get(getUsers)
+
+router.route("/login").post(loginUser)
+router.route("/register").post(addUser)
+
+
+
+module.exports = router
