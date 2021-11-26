f2f09c2c1cee8369b90c6697119985cfbac9a292
Eugene Korir <korir16071@gmail.com>
2021-11-26 17:09:11 +0300

    Modified Admin.jsx, App.js and added some styles in App.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,23 @@ import Signup from './components/Signup';
 import { BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from './components/Home';
 import Login from "./components/Login"
-import Users from './components/Users';
+import Error from './components/Error';
 import Admin from './components/Admin';
+import Header from './components/Header';
 
 
 const App = ()=> {
   return (
     <BrowserRouter>
     <div className="App">
+    <Header/>
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/register" element={<Signup/>}/>
       <Route path="/login" element={<Login/>}/>
-      <Route path="/users" element={<Users/>} />
+      {/* <Route path="/users" element={<Users/>} /> */}
       <Route path="/admin" element={ <Admin/>} />
+      <Route path="*" element={<Error/>} />
     </Routes>
     
 
