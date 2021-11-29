027bd9b5c0298a4b747b15bc1a9f285631258284
Palpatine <korir16071@gmail.com>
2021-11-29 17:29:47 +0300

    Modified Admin.jsx and Error.jsx

--- frontend/src/components/Error.jsx
+++ frontend/src/components/Error.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Error = () => {
     return (
         <div className="e404">
-            <p>404: page not found</p>
+            <p className="error-msg">404: page not found</p>
         </div>
     )
 }
