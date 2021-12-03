5fb3101080a338e2c25432141530dc87c97fb809
Palpatine <korir16071@gmail.com>
2021-12-03 17:43:15 +0300

    added routes in App.js and styles in App.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,21 +6,35 @@ import Login from "./components/Login"
 import Error from './components/Error';
 import Admin from './components/Admin';
 import Header from './components/Header';
+import Users from './components/Users';
+import SideBar from './components/SideBar';
+import Projects from './components/Projects';
 
 
 const App = ()=> {
+  // const {user} = useSelector(state => state.user)
   return (
     <BrowserRouter>
     <div className="App">
     <Header/>
     <Routes>
-      <Route path="/" element={<Home/>}/>
       <Route path="/register" element={<Signup/>}/>
       <Route path="/login" element={<Login/>}/>
+    </Routes>
+    <div className="admin-page">
+
+    <SideBar/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      
       {/* <Route path="/users" element={<Users/>} /> */}
       <Route path="/admin" element={ <Admin/>} />
       <Route path="*" element={<Error/>} />
+      <Route path="/admin/users" element={<Users/>}/>
+      <Route path="/admin/projects" element={<Projects/>}/>
     </Routes>
+    </div>
+    {/* {user.isAdmin && <SideBar/>} */}
     
 
     </div>
