173dc83cd1330692ae12f91b57fdfd4a33ab90b9
Palpatine <korir16071@gmail.com>
2021-12-12 21:55:57 +0300

    added pages

---
+++ frontend/src/components/pages/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Error = () => {
+    return (
+        <div className="e404 admin">
+            <p className="error-msg">404: page not found</p>
+        </div>
+    )
+}
+
+export default Error
---
+++ frontend/src/components/pages/User.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router';
+
+
+const User = () => {
+    const {users} = useSelector(state => state.user)
+    const {id}= useParams()
+
+    const user = users.find(user=>user.id === parseInt(id))
+    console.log(user);
+
+    return (
+        <div className="admin">
+        {/* {user.map(user=>( */}
+            <div>
+            <p>{user.fullname}</p>
+            <p>Email: {user.email}</p>
+            <p>{user.password}</p>          
+
+
+            </div>
+        {/* ))} */}
+        </div>
+    )
+}
+
+export default User
