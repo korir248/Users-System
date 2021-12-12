f0eff00a58e5acce1f4c7938d6550eb3d15cba8f
Palpatine <korir16071@gmail.com>
2021-12-12 21:54:59 +0300

    modiifed index.js

--- background services/index.js
+++ background services/index.js
@@ -1,9 +1,15 @@
 require('dotenv').config()
 const express = require('express')
 const PORT = process.env.PORT
-const cron = require('cron')
+const cron = require('node-cron')
 
 const app = express()
+
+const run = async()=>{
+    cron.schedule(' * * * * * *',async()=>{
+        console.log('1');
+    })
+}
 
 app.listen(PORT,()=>{
     console.log(`App running on port ${PORT}`);
