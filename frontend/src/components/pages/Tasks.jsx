d3b8c197fd06911ef525e580b31dc3a794d3c298
Palpatine <korir16071@gmail.com>
2021-12-15 17:06:08 +0300

    modified Task

--- frontend/src/components/pages/Tasks.jsx
+++ frontend/src/components/pages/Tasks.jsx
@@ -1,25 +1,50 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import DeleteIcon from '@mui/icons-material/Delete'
+import Checkbox from '@mui/material/Checkbox'
+import { deleteTask, getTasks } from '../../redux/actions/taskActions'
+import CreateTask from '../mini components/CreateTask'
 
 const Tasks = () => {
 
     const { tasks} = useSelector(state => state.task)
+    const dispatch = useDispatch()
+
+    const deletingTask = (id)=>{
+        console.log("deleting...");
+        dispatch(deleteTask(id))
+    }
+    useEffect(() => {
+        dispatch(getTasks())
+    }, [dispatch, tasks])
 
     return (
         <div className="admin">
             <p>Tasks</p>
+            <CreateTask/>
             {tasks.length ? 
             <>
             <table>
-            <thead></thead>
+            <thead>
+                <tr>
+                    <td>Task Name</td>
+                    <td>Project</td>
+                    <td>isAssigned</td>
+                    <td>Status</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+
+            </thead>
             <tbody>
             {tasks.map(task=> (
                 <tr>
-                <Link to={`/admin/users/${task.id}`}>
-                <td>{task.fullname}</td>
-                <td>{task.email}</td>
-                </Link>
+                <td>{task.task_name}</td>
+                <td>{task.project_name}</td>
+                <td>{task.isAssigned.toString()}</td>
+                <td>{task.isCompleted ? "Completed" : "OnGoing"}</td>
+                <td><DeleteIcon onclick={()=> deletingTask(task.id)}/> </td>
+                <td><Checkbox /></td>
                 </tr>
         )
         )}
