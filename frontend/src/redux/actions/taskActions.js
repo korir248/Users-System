0b0782068ae06752bbefce25f9539094f9407996
Palpatine <korir16071@gmail.com>
2021-12-15 17:05:38 +0300

    modified task actions

--- frontend/src/redux/actions/taskActions.js
+++ frontend/src/redux/actions/taskActions.js
@@ -1,4 +1,4 @@
-import { GET_TASKS_FAILED, GET_TASKS_REQUEST, GET_TASKS_SUCCESS } from "../types"
+import { DELETE_TASK_FAILED, DELETE_TASK_SUCCESS, GET_TASKS_FAILED, GET_TASKS_REQUEST, GET_TASKS_SUCCESS } from "../types"
 import axios from 'axios'
 
 
@@ -34,4 +34,32 @@ export const getTasks = ()=> async(dispatch)=>{
             payload: error.message
         })
     }
+}
+
+export const deleteTask = (id)=> async(dispatch)=>{
+    try {
+        let token = localStorage.getItem('token')
+        const config = {
+            headers: {
+                'Content-type': 'application/json',
+                'Authorization': token
+            }
+
+        }
+        const {data} = await axios.post("http://localhost:3002/admin/tasks",{id},config)
+
+        dispatch({
+            type: DELETE_TASK_SUCCESS,
+            payload: data
+        })
+
+        
+    } catch (error) {
+        dispatch({
+            type: DELETE_TASK_FAILED,
+            payload: error.message
+        })
+        
+    }
+
 }
