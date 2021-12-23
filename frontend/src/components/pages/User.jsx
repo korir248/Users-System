c8b702c34ead239f7657622038f29145db5efa1d
Eugene Korir <62384233+korir248@users.noreply.github.com>
2021-12-23 17:57:36 +0300

    Modified User.jsx

--- frontend/src/components/pages/User.jsx
+++ frontend/src/components/pages/User.jsx
@@ -6,6 +6,8 @@ import { useParams } from 'react-router';
 const User = () => {
     const {users} = useSelector(state => state.user)
     const {id}= useParams()
+    const {tasks} = useSelector(state => state.task)
+    const specTasks  =  tasks.filter(task=> task.email === user.email)
 
     const user = users.find(user=>user.id === parseInt(id))
     console.log(user);
@@ -21,6 +23,49 @@ const User = () => {
 
 
             </div>
+         </p>
+        {specTasks.length ? 
+        <>
+        <p>
+            You are currently working on the following {specTasks.length === 1 ? 'task' : 'tasks'}:
+        </p>
+            <table>
+            <thead>
+                <tr>
+                    {/* <td>#</td> */}
+                    <td>Task</td>
+                    <td>Project</td>
+                    <td>isSubmitted</td>
+                    <td>Status</td>
+                    {/* <td></td> */}
+                    {/* <td></td> */}
+                    {/* <td></td> */}
+                </tr>
+
+            </thead>
+            <hr/>
+            <tbody>
+            {specTasks.map((task,index)=> (
+                <tr key={task.id}>
+                {/* <td>{index +1}</td> */}
+                <td>{task.task_name}</td>
+                <td>{task.project_name}</td>
+                {/* <td>{task.isAssigned.toString()}</td> */}
+                <td>{task.isCompleted ? "Yes" : "Not Yet"}</td> 
+                 {/* <td>{!task.isAssigned ? <DeleteIcon sx={{ color: red[500] }} className="delete-btn" onClick={()=> deletingTask(task.id)}/> : 
+                    <DeleteIcon sx={{ color: red[500] }} disabled/> }</td> */}
+                <td>{!task.isSubmitted ? <FormControlLabel control={ <Checkbox onChange={handleChange} value={task.id}/>} label="Submit"/>: "Submitted"}</td> 
+                {/* <td>{!task.isAssigned ? <AssignTask task_id={task.id} project_id={task.project_id}/> : <UnAssignTask task_id={task.id}/>}</td> */}
+                </tr>
+        )
+        )}
+        </tbody>
+        </table>
+        </>
+            : 
+            <>
+            <p>{user.fullname} hasn't been assigned any task!</p>
+            </>
         {/* ))} */}
         </div>
     )
