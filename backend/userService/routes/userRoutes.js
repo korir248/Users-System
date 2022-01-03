57b58e507868c4cbfa6dbdea6adb12748999ecc8
Palpatine <korir16071@gmail.com>
2022-01-03 18:34:07 +0300

    updated userService

--- backend/userService/routes/userRoutes.js
+++ backend/userService/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const auth = require('../middlewares/auth')
-const { getUsers, loginUser, addUser, deleteUser } = require('../controllers/usersController')
+const { getUsers, loginUser, addUser, deleteUser, sendEmailRegister } = require('../controllers/usersController')
 const router =  express.Router()
 
 
@@ -8,6 +8,8 @@ router.route("/users").get(auth,getUsers)
 router.route("/login").post(loginUser)
 router.route("/register").post(addUser)
 router.route("/admin/users").delete(auth,deleteUser)
+
+router.route("/email").get(sendEmailRegister)
 
 
 module.exports = router
